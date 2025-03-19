ae9aba24927256c782bd03f475e6275627685390
Cauê Alves <cauealvesb4@gmail.com>
2025-03-19 20:08:47 -0300

    Invertando uma palavra

--- Lógica/Dia 3/index.js
+++ Lógica/Dia 3/index.js
@@ -1,5 +1,20 @@
-let varText = "2";
-let varNumber = 2;
-let varBoolen = true;
-let varNull = null;
-let varUndefined = undefined;
+// let varText = "2";
+// let varNumber = 2;
+// let varBoolen = true;
+// let varNull = null;
+// let varUndefined = undefined;
+
+let palavra = "Geovanna"
+let arr = palavra.split("")
+
+let newArray = []
+for(let i = 0; i < arr.length; i++){
+    newArray.unshift(arr[i])
+}
+
+let newPalavra = ""
+for(let i = 0; i < newArray.length; i++){
+    newPalavra += newArray[i]
+}
+
+console.log(newPalavra)
