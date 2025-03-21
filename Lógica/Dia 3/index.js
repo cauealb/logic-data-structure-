3c8f32cdf92ae924967cb26b6d7643351ef91796
Cauê Alves <cauealvesb4@gmail.com>
2025-03-21 00:35:57 -0300

    Finalizando o fibonacci

--- Lógica/Dia 3/index.js
+++ Lógica/Dia 3/index.js
@@ -1,17 +1,29 @@
 // Crie uma função que retorne os primeiros n números da sequência de Fibonacci.
 // Exemplo: n = 6 → [0, 1, 1, 2, 3, 5]
 
+// const fibonacci = (n) => {
+//     let finoArray = [0, 1]
+//     let ant1 = 0
+//     let ant2 = 1
+//     let num = 0
+//     for(let i = 0; i < n; i++){
+//         num += ant1 + ant2
+//         ant1 = finoArray[i - 2]
+//         ant2 = finoArray[i - 1]
+//         finoArray.push(num)
+//     }
+// }
+
+// console.log(fibonacci(6))
+
 const fibonacci = (n) => {
-    let finoArray = [0, 1]
-    let ant1 = 0
-    let ant2 = 1
-    let num = 0
-    for(let i = 0; i < n; i++){
-        num += ant1 + ant2
-        ant1 = finoArray[i - 2]
-        ant2 = finoArray[i - 1]
-        finoArray.push(num)
+    const sequencia = [0, 1]
+
+    for(let i = 2; i < n; i++) {
+        sequencia.push(sequencia[i - 1] + sequencia[i - 2])
     }
+
+    return sequencia.slice(0, n)
 }
 
 console.log(fibonacci(6))
