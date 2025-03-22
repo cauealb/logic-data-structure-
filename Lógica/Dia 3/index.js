c11bbc07d782484b17f2c83b7f461a5367145683
Cauê Alves <cauealvesb4@gmail.com>
2025-03-22 11:04:25 -0300

    recursão fatorial

--- Lógica/Dia 3/index.js
+++ Lógica/Dia 3/index.js
@@ -1,8 +1,12 @@
-function somaArray(arr) {
-    if(arr.length === 0) {
+function fatorial(n) {
+    if(n === 0) {
         return 0
     }
-    return arr[0] + somaArray(arr.slice(1))
+    if (n === 1) {
+        return 1
+    }
+
+    return n * fatorial(n - 1)
 }
 
-console.log(somaArray([1, 2, 3, 4]))
+console.log(fatorial(5))
