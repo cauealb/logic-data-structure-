f469548a27e73357cfc70ecd3eadfadbfbda6261
Cauê Alves <cauealvesb4@gmail.com>
2025-03-22 10:54:12 -0300

    Somando todos os números de um array

--- Lógica/Dia 3/index.js
+++ Lógica/Dia 3/index.js
@@ -1,9 +1,8 @@
-function soma(n) {
-    if(n === 1) {
-        return  1
+function somaArray(arr) {
+    if(arr.length === 0) {
+        return 0
     }
-
-    return n + soma(n - 1)
+    return arr[0] + somaArray(arr.slice(1))
 }
 
-console.log(soma(10))
+console.log(somaArray([1, 2, 3, 4]))
